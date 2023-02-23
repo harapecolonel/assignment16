ec68d52800d06cf1fe7f0f4e45a79b4d5eb214ca
harapecolonel <yuricolor03@gmail.com>
2023-02-23 21:01:23 +0000

    homework17

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function(){
+    Route::get('news/create','add');
+});
+
+Route::controller(AAA::class)->group(function() {
+    Route::get(' ', 'bbb');
+});
+
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
+

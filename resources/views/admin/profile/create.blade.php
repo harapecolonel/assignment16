158a7109771f5a6410236ad31526e64649ea04f5
harapecolonel <yuricolor03@gmail.com>
2023-03-02 20:22:51 +0000

    3/3

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,18 @@
+{{-- resources/views/layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'プロフィールの作成'を埋め込む --}}
+@section('title', 'プロフィールの作成')
+
+
+{{--profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div> 
+    @endsection
+    
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\NewsController;
+use App\Http\Controllers\Admin\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +19,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function(){
     Route::get('news/create','add');
 });
 
-Route::controller(AAA::class)->group(function() {
-    Route::get(' ', 'bbb');
-});
 
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
     Route::get('profile/create', 'add');
     Route::get('profile/edit', 'edit');
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
